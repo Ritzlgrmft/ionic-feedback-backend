752640479fb91f4afb96eb3d7a8d5694335f4c39
Ritzlgrmft <markus.wagner02@web.de>
2017-05-17 18:25:07 +0200

    fix(settings): split settings

--- Markus.Feedback.Backend/Startup.cs
+++ Markus.Feedback.Backend/Startup.cs
@@ -8,67 +8,69 @@ using Swashbuckle.AspNetCore.Swagger;
 
 namespace Markus.Feedback.Backend
 {
-	public class Startup
-	{
-		public Startup(IHostingEnvironment env)
-		{
-			Configuration = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-				.AddEnvironmentVariables()
-				.Build();
-		}
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("Settings/logging.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Settings/mail.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Settings/registration.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
 
-		public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc();
-			services.AddCors(options =>
-			{
-				options.AddPolicy("CorsPolicy",
-					builder => builder
-					.AllowAnyOrigin()
-					.WithHeaders("authorization", "content-type")
-					.AllowCredentials()
-					.WithMethods("POST")
-				);
-			});
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .WithHeaders("authorization", "content-type")
+                    .AllowCredentials()
+                    .WithMethods("POST")
+                );
+            });
 
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new Info { Title = "Markus.Feedback.Backend", Version = "v1" });
-			});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Markus.Feedback.Backend", Version = "v1" });
+            });
 
-			services.AddOptions();
-			services.Configure<MailConfiguration>(Configuration.GetSection("Mail"));
-			services.Configure<RegistrationConfiguration>(Configuration.GetSection("Registration"));
-		}
+            services.AddOptions();
+            services.Configure<MailConfiguration>(Configuration.GetSection("Mail"));
+            services.Configure<RegistrationConfiguration>(Configuration.GetSection("Registration"));
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-		{
-			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-			loggerFactory.AddDebug();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
-			app.UseSwagger();
-			app.UseSwaggerUi(c =>
-			{
-				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Markus.Feedback.Backend V1");
-			});
-		}
-	}
+            app.UseSwagger();
+            app.UseSwaggerUi(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Markus.Feedback.Backend V1");
+            });
+        }
+    }
 }
